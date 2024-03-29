236a47df96521412b3ce5dbe2ea39f44742f0cb8
semlaan <semlaan@dds.nl>
2024-03-29 19:28:03 +0100

    added comments to render target

--- src/renderer/render_target.h
+++ src/renderer/render_target.h
@@ -4,10 +4,15 @@
 #include "renderer_types.h"
 
 
+// Creates a handle to a render target
 RenderTarget RenderTargetCreate(u32 width, u32 height, RenderTargetUsage colorBufferUsage, RenderTargetUsage depthBufferUsage);
+// Destroys the given render target
 void RenderTargetDestroy(RenderTarget clientRenderTarget);
 
+// All following draw commands will be drawn to this render target 
+// NOTE: no other render target needs to be rendering before calling this function
 void RenderTargetStartRendering(RenderTarget clientRenderTarget);
+// Ends rendering and resolves the render target
 void RenderTargetStopRendering(RenderTarget clientRenderTarget);
 
 Texture GetColorAsTexture(RenderTarget clientRenderTarget);
