52f33892d63deb48ea3702b216a6a544ba416890
semlaan <semlaan@dds.nl>
2024-12-28 16:29:12 +0100

    Added comments to marching cubes

--- src/game/marching_cubes.h
+++ src/game/marching_cubes.h
@@ -1,10 +1,15 @@
 #pragma once
 #include "renderer/material.h"
 
+
+/// @brief Generates 3d floating point density array that defines the terrain.
 void MCGenerateDensityMap();
 
+/// @brief Generates a mesh for the generated density map. Density map is assumed to be generated already
 void MCGenerateMesh();
 
+/// @brief Renders the mesh, assumes that the mesh has been generated.
 void MCRenderWorld();
 
+/// @brief Cleans up all marching cubes resources.
 void MCDestroyMeshAndDensityMap();
